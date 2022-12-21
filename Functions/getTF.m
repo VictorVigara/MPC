3314843b143473a74a8c948104635faaba5107ff
Victor Vigara <vigaravictor99@gmail.com>
2022-12-21 14:00:59 +0100

    comments

--- Functions/getTF.m
+++ Functions/getTF.m
@@ -14,7 +14,7 @@ for i=1:size(norm_step,2)
             time_step = time(idx); 
         end
         y_ss(j,i) = max(y_step);            % Get steady state gain
-        y_time_constant = 0.632*y_ss(j,i);  % Get gain to determine time constant
+        y_time_constant = 0.632*y_ss(j,i);  % Get gain at 63.2% to determine time constant
         % Get time constant in seconds by interpolating previous value  
         time_constant(j,i) = interp1(y_step, time_step, y_time_constant) - time_step(1);
         fprintf("Transfer function from F1 to Y%d - Step = %f", i, steps(j))
